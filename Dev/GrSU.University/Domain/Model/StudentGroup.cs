2615d0abe79ddbca248698095e6848dced7584a1
Sergey Vishnevsky <svishnevskij@gmail.com>
2015-09-30 22:36:44 +0300

    Fixed domain model

--- Dev/GrSU.University/Domain/Model/StudentGroup.cs
+++ Dev/GrSU.University/Domain/Model/StudentGroup.cs
@@ -1,17 +1,9 @@
 ﻿namespace GrSU.University.Domain.Model
 {
-    using System.Collections.Generic;
     using Common;
 
     public class StudentGroup : BaseModel
     {
         public string Name { get; set; }
-
-        public List<Student> StudentList { get; set; }
-
-        public StudentGroup()
-        {
-            this.StudentList = new List<Student>();
-        }
     }
 }
