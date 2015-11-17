694256048604b3e797dc039f7d43264c92f22543
unknown <s.vishnevsky@VISHNEVSKYS.itransition.corp>
2015-11-17 11:20:11 +0300

    Implemented common async domain service

---
+++ Dev/GrSU.University.Domain.Services.Static/Common/DomainServiceAsync.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrSU.University.Domain.Common;
+using GrSU.University.Domain.Model.Common;
+
+namespace GrSU.University.Domain.Services.Static.Common
+{
+    public abstract class DomainServiceAsync<T> : DomainService<T>, IDomainServiceAsync<T> where T : BaseModel
+    {
+        private readonly int delay;
+
+        public DomainServiceAsync() : this(5000)
+        {
+
+        }
+
+        public DomainServiceAsync(int delay)
+        {
+            this.delay = delay;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await Task.Delay(delay);
+
+            return base.Add(entity);
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            await Task.Delay(delay);
+
+            return base.Get(id);
+        }
+
+        public async Task<List<T>> GetAsync()
+        {
+            await Task.Delay(delay);
+
+            return base.Get();
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            await Task.Delay(delay);
+
+            return base.Update(entity);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await Task.Delay(delay);
+
+            base.Delete(id);
+        }
+    }
+}
