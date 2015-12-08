ef4a298051da10e005e17167f1a881722cfba92d
unknown <s.vishnevsky@VISHNEVSKYS.itransition.corp>
2015-12-08 12:03:17 +0300

    Added audit manager contract

---
+++ Dev/GrSU.University/Audit/DataAccessEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrSU.University.Audit
+{
+    public class DataAccessEventArgs : EventArgs
+    {
+    }
+}
---
+++ Dev/GrSU.University/Audit/IAuditManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrSU.University.Audit
+{
+    public delegate void DataAccessDelegate(object sender, DataAccessEventArgs eventArgs);
+
+    public interface IAuditManager
+    {
+        event DataAccessDelegate OnAccess;
+
+        void Access();
+    }
+}
