2b73545af52ec103993ac2df5e72f9d871a615b4
Sergey Vishnevsky <Sergey Vishnevsky>
2016-05-18 22:04:15 +0300

    Маппинг отделен от контролеров

--- Dev/GrSU.University.Clients.Web/Controllers/StudentGroups/StudentGroupsController.cs
+++ Dev/GrSU.University.Clients.Web/Controllers/StudentGroups/StudentGroupsController.cs
@@ -8,20 +8,9 @@ namespace GrSU.University.Clients.Web.Controllers.StudentGroups
 
     public class StudentGroupsController : BaseListController<IStudentGroupServiceAsync, StudentGroup, StudentGroupModel, StudentGroupListModel>
     {
-        private readonly IStudentServiceAsync studentService;
-
-        public StudentGroupsController(IStudentGroupServiceAsync studentGroupService, IStudentServiceAsync studentService, IMapper mapper)
+        public StudentGroupsController(IStudentGroupServiceAsync studentGroupService, IMapper mapper)
             : base(studentGroupService, mapper)
         {
-            this.studentService = studentService;
-        }
-
-        protected override StudentGroupListModel MapListModel(StudentGroup entity)
-        {
-            var model = base.MapListModel(entity);
-            model.StudentCount = studentService.GetByGroupId(entity.Id).Result.Count;
-
-            return model;
         }
     }
 }
---
+++ Dev/GrSU.University.Clients.Web/Mapping/RoomProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GrSU.University.Clients.Web.Models.Rooms;
+using GrSU.University.Domain.Model;
+
+namespace GrSU.University.Clients.Web.Mapping
+{
+    public class RoomProfile : Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<Room, RoomModel>()
+                .ReverseMap();
+
+            CreateMap<Room, RoomListModel>();
+        }
+    }
+}
